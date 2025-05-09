f33941040652cf69d3d6db93810dd43243461177
RajSingh313 <123961810+RajSingh313@users.noreply.github.com>
2025-05-09 09:04:47 +0500

    Initial commit - multi-module Spring Boot project

---
+++ tracking-api-module/src/main/java/com/raj/mutlimodule/MutlimoduleApplication.java
@@ -0,0 +1,13 @@
+package com.raj.mutlimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutlimoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MutlimoduleApplication.class, args);
+    }
+
+}
---
+++ tracking-api-module/src/test/java/com/raj/mutlimodule/MutlimoduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.mutlimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutlimoduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
